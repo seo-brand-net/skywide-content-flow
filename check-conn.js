cf0337cf18e6c878bab82a8ea74f7650d086b651
Mo Awwal <gomandev@gmail.com>
2026-03-12 15:28:20 +0000

    feat: retry

--- check-conn.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const FILE = 'DEV Skywide  Content (TEST v23).json';
-const data = JSON.parse(fs.readFileSync(FILE, 'utf8'));
-
-console.log('\\nExecution Data1 Connections:');
-console.dir(data.connections['Execution Data1'], { depth: null });
